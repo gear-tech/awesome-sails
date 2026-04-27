e2bf75896f6c47f319680348b35889e0ce58b3a2
Mansur Azatbek <mansur62624@gmail.com>
2026-04-27 10:35:31 +0500

    chore: minor updates

--- utils/src/math.rs
+++ utils/src/math.rs
@@ -27,11 +27,7 @@ use bnum::BUintD8;
 use core::cmp::Ordering;
 use derive_more::Deref;
 use parity_scale_codec::{Decode, Encode};
-use sails_type_registry::{
-    TypeBuilder, TypeInfo,
-    ast::{PrimitiveType, Type, TypeDecl},
-    const_suffixed_name,
-};
+use sails_type_registry::{TypeInfo, ast::TypeDecl, const_suffixed_name};
 
 pub use primitive_types::{H160, H256, U256};
 
@@ -390,38 +386,14 @@ impl<const N: usize> Decode for LeBytes<N> {
 impl<const N: usize> TypeInfo for LeBytes<N> {
     type Identity = Self;
 
-    fn module_path() -> &'static str {
-        module_path!()
-    }
-
     fn type_decl(registry: &mut sails_type_registry::Registry) -> TypeDecl {
-        let base_name = const_suffixed_name(
-            "LeBytes",
-            alloc::vec![(alloc::string::String::from("N"), alloc::format!("{N}"))],
-        );
-        registry.register_named_type_with_dependencies(
+        registry.register_named_type(
             Self::META,
-            base_name,
+            const_suffixed_name(
+                "LeBytes",
+                alloc::vec![(alloc::string::String::from("N"), alloc::format!("{N}"))],
+            ),
             alloc::vec![],
-            |_registry| {},
-        )
-    }
-
-    fn type_def(_registry: &mut sails_type_registry::Registry) -> Option<Type> {
-        let base_name = const_suffixed_name(
-            "LeBytes",
-            alloc::vec![(alloc::string::String::from("N"), alloc::format!("{N}"))],
-        );
-        Some(
-            TypeBuilder::new()
-                .name(base_name)
-                .composite()
-                .unnamed()
-                .ty(TypeDecl::Array {
-                    item: alloc::boxed::Box::new(TypeDecl::Primitive(PrimitiveType::U8)),
-                    len: N as u32,
-                })
-                .build(),
         )
     }
 }
